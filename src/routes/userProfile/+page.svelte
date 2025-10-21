785481f0511f13e8e9f8e8a31ddeb21e106dcc7d
mnageswar <mani.nageswar@amadeus.com>
2025-10-22 00:24:03 +0530

    feat : added form validations for user profile page

--- src/routes/userProfile/+page.svelte
+++ src/routes/userProfile/+page.svelte
@@ -7,6 +7,9 @@
     let { data } = $props();
     let editingProfile = $state(false);
     let userName;
+    let formErrors = $state({});
+    let isUpdateProfileButtonDisabled = $state(true);
+    let formElement;
 
     function handleSubmit() {
         return async ({ result }) => {
@@ -23,49 +26,95 @@
             }
         }
     }
-</script>
 
-{#snippet infoInputTile(name,type,label,value)}
-    <div class="border-3 border-pg-sky-light {editingProfile ? "has-[:focus]:border-pg-sky" : ""} rounded-full h-16 mb-8 flex items-center px-6 relative">
-        <label for={name} class="absolute bottom-12 bg-white text-xl">{label}<span class="text-red-500 {!editingProfile ? 'hidden' : ''}">*</span></label>
-        {#if name == "name"}
-            <input class="w-full font-Manrope text-pg-sky-text text-lg border-none focus:ring-0 p-0" required bind:this={userName} readonly={!editingProfile} {name} {value} {type}>
-        {:else}
-            <input class="w-full font-Manrope text-pg-sky-text text-lg border-none focus:ring-0 p-0" required readonly={!editingProfile} {name} {value} {type}
-            onkeydown={(e) => { 
-                if ((name == 'number' || name == 'emergencyContact') &&
-                (e.target.value.length >= 10 && !['Backspace', 'Delete', 'ArrowLeft', 'ArrowRight'].includes(e.key)) ||
-                ['e', ' ', '+', '-', '.'].includes(e.key)) {
-                    e.preventDefault();
-                }
-                }}
-            >
-        {/if}
-        
-    </div>
-{/snippet}
+    function validateMobileNumber(event) {
+        const input = event.target;
+        const mobileNumber = input.value;
+        const mobileNumberPattern = /^[0-9]{10}$/;
+
+        if (!mobileNumberPattern.test(mobileNumber)) {
+            formErrors[input.name] = 'enter a valid 10 digit mobile number';
+            console.log("formErrors",formErrors);
+        } else {
+            delete formErrors[input.name];
+        }
+    }
+
+    function validateEmail(event) {
+        const input = event.target;
+        const email = input.value;
+        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        if (!emailPattern.test(email)) {
+            formErrors[input.name] = 'enter a valid email address';
+        } else {
+            delete formErrors[input.name];
+        }
+    }
+
+    function isProfileEdited() {
+        const formDataObjInEdit = Object.fromEntries(new FormData(formElement).entries());
+        isUpdateProfileButtonDisabled = Object.keys(formDataObjInEdit).every(
+            key => formDataObjInEdit[key] == String(data.userProfile[key])
+        );
+    }
+</script>
 
 <h2 class="font-Manrope">user profile</h2>
 
-<form method="POST" action="?/updateUserProfile" use:enhance={handleSubmit}>
+<form method="POST" action="?/updateUserProfile" use:enhance={handleSubmit} bind:this={formElement} enctype="multipart/form-data" oninput={isProfileEdited}>
 
     <div class="flex flex-col items-center p-5 mb-4">
-        <img src="/favicon.svg" alt="" class="border border-pg-sky rounded-full shadow-xl w-[160px] h-[160px] mb-6"/>
+        <img src="/images/pgProfile.png" alt="" class="border border-pg-sky rounded-full shadow-xl w-[160px] h-[160px] mb-6"/>
         <h3 class="font-Manrope text-pg-sky-text">{data.userProfile.name}</h3>
     </div>
 
     <hr class="border border-pg-sky-light mb-10">
 
-    {@render infoInputTile("name","text","name",data.userProfile.name)}
+    <div class="border-3 border-pg-sky-light {editingProfile ? "has-[:focus]:border-pg-sky" : "shadow-xl"} rounded-full h-16 mb-8 flex items-center px-6 relative">
+        <label for="name" class="absolute bottom-12 bg-white text-xl">name<span class="text-red-500 {!editingProfile ? 'hidden' : ''}">*</span></label>
+        <input class="w-full font-Manrope text-pg-sky-text text-lg border-none focus:ring-0 p-0"
+            readonly={!editingProfile} name="name" bind:this={userName} value={data.userProfile.name} type="text">
+    </div>
 
-    {@render infoInputTile("email","email","email",data.userProfile.email)}
-
-    {@render infoInputTile("number","number","mobile no.",data.userProfile.number)}
-
-    {@render infoInputTile("emergencyContact","number","emergency contact no.",data.userProfile.emergencyContact)}
+    <div class="border-3 border-pg-sky-light
+        {editingProfile ? (formErrors?.email ? "has-[:focus]:border-pg-red-button" : "has-[:focus]:border-pg-sky") : "shadow-xl"}
+        rounded-full h-16 {formErrors?.email ? "mb-1" : "mb-8"} flex items-center px-6 relative">
+        <label for="email" class="absolute bottom-12 bg-white text-xl">email<span class="text-red-500 {!editingProfile ? 'hidden' : ''}">*</span></label>
+        <input class="w-full font-Manrope text-pg-sky-text text-lg border-none focus:ring-0 p-0"
+            readonly={!editingProfile} name="email" value={data.userProfile.email} type="email"
+            oninput={validateEmail}>
+    </div>
+    <span class="text-pg-red text-sm {formErrors?.email ? "mb-10" : "hidden"} px-6">{formErrors?.email}</span>
     
-    <button class="mt-5 bg-pg-sky text-white px-4 py-2 rounded-md w-full flex items-center justify-center gap-1 cursor-pointer"
-        type={!editingProfile ? "notbutton" : "button"} onclick={() => {
+    <div class="border-3 border-pg-sky-light {formErrors?.email ? "mt-4" : ""}
+        {editingProfile ? (formErrors?.number ? "has-[:focus]:border-pg-red-button" : "has-[:focus]:border-pg-sky") : "shadow-xl"}
+        rounded-full h-16 {formErrors?.number ? "mb-1" : "mb-8"} flex items-center px-6 relative">
+        <label for="number" class="absolute bottom-12 bg-white text-xl">mobile no.<span class="text-red-500 {!editingProfile ? 'hidden' : ''}">*</span></label>
+        <input class="w-full font-Manrope text-pg-sky-text text-lg border-none focus:ring-0 p-0" 
+            readonly={!editingProfile} name="number" value={data.userProfile.number} type="number"
+            oninput={validateMobileNumber}
+            onwheel={(e) => e.target.blur()}
+            onkeydown={(e) => { preventKeyPress(e, ['e', ' ', '+', '-', '.'])}}>
+    </div>
+    <span class="text-pg-red text-sm {formErrors?.number ? "mb-10" : "hidden"} px-6">{formErrors?.number}</span>
+
+    <div class="border-3 border-pg-sky-light {formErrors?.number ? "mt-4" : ""}
+        {editingProfile ? (formErrors?.emergencyContact ? "has-[:focus]:border-pg-red-button" : "has-[:focus]:border-pg-sky") : "shadow-xl"}
+        rounded-full h-16 {formErrors?.emergencyContact ? "mb-1" : "mb-8"} flex items-center px-6 relative">
+        <label for="emergencyContact" class="absolute bottom-12 bg-white text-xl">emergency contact no.<span class="text-red-500 {!editingProfile ? 'hidden' : ''}">*</span></label>
+        <input class="w-full font-Manrope text-pg-sky-text text-lg border-none focus:ring-0 p-0"
+            readonly={!editingProfile} name="emergencyContact" value={data.userProfile.emergencyContact} type="number"
+            oninput={validateMobileNumber}
+            onwheel={(e) => e.target.blur()}
+            onkeydown={(e) => { preventKeyPress(e, ['e', ' ', '+', '-', '.'])}}>
+    </div>
+    <span class="text-pg-red text-sm {formErrors?.emergencyContact ? "mb-8" : "hidden"} px-6">{formErrors?.emergencyContact}</span>
+    
+    <button class="mt-5 pg-sky-button w-full flex items-center justify-center gap-1 cursor-pointer"
+        type={!editingProfile ? "notbutton" : "button"}
+        disabled={editingProfile && isUpdateProfileButtonDisabled}
+        onclick={() => {
             editingProfile = !editingProfile
             tick().then(() => {userName?.focus()});
             }}>
